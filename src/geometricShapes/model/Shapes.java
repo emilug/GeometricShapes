30270dfe46edc1d79a5505d6341451628ef8c4da
Cyrus <Cyrus@Cyrus-PC.home>
2019-04-17 19:13:08 +0300

    1

--- src/geometricShapes/model/Shapes.java
+++ src/geometricShapes/model/Shapes.java
@@ -12,26 +12,7 @@ import geometricShapes.model.impl.Triangle;
 public class Shapes {
 
 	public static void main(String[] args) {
-//		// method 1:
-//		Circle aCircle = new Circle(3);
-//		Square aSquare = new Square(4);
-//
-//		// method 2 - here we have an example of polimorphism (forced):
-//		Shape shape1 = new Circle(3);
-//		Shape shape2 = new Square(4);
-//		System.out.println("'aCircle' has the area: " + aCircle.calculateArea());
-//		System.out.println("'aSquare' has the perimeter: " + aSquare.calculatePerimeter());
-//		System.out.println("'shape1' has the perimeter: " + shape1.calculatePerimeter());
-//		System.out.println("'shape2' has the area: " + shape2.calculateArea());
 
-		// Homework:
-		// Complete the program by adding a console menu
-		// The menu should list the type of shapes
-		// The menu should allow the user to select a specific shape ( you can use an
-		// index to specify the option)
-		// The menu should then ask for shape values (side, radius etc)
-		// At the end the program computes the area & perimeter with the supplied values
-		
 		System.out.println("Please select the type of shape:");
 		System.out.println("1. Circle");
 		System.out.println("2. Square");
